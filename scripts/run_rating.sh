fec367348d0f10eebce094f71b2b6fc9281c3f51
Min KyungHyun <khmin1104@gmail.com>
2023-11-26 02:11:57 +0900

    feat(data):

---
+++ scripts/run_rating.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+# Specify your API key first.
+
+# Specify your EXEHOME first. EXEHOME=/home/user-name/redefine_prompter
+cd ${EXEHOME}
+
+
+python3 rating.py \
+        --backbone 'chatgpt' \
+        --temperature 0.5 \
+        --sc_num 5 \
+        --output_dir 'output/' \
+        --key ${APIKEY} \
+        # --start 6 \
+        # --end 7
+
